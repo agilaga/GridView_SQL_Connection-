2d096d6246de696ddf3f46ae8876ea419d6f66bb
AGIL AGHAZADA <aqil.agazade.97@gmail.com>
2022-11-27 00:08:20 +0400

    Add project files.

---
+++ View SQL connection product category/View SQL.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="View SQL.aspx.cs" Inherits="View_SQL_connection_product_category.View_SQL" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Agil Gridview</title>
+</head>
+<link href="style.css" rel="stylesheet" />
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbl1" runat="server" Text="CategoryName"></asp:Label>
+            <asp:TextBox ID="txbox1" runat="server" OnTextChanged="txbox1_TextChanged"></asp:TextBox>
+        </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server" CssClass="mydatagrid" PagerStyle-CssClass="pager"
+                HeaderStyle-CssClass="header" RowStyle-CssClass="rows"></asp:GridView>
+        </div>
+        <div>
+            <asp:Label ID="lbl2" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:Button ID="btn1" runat="server" CssClass="button" Text="Search" OnClick="btn1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ View SQL connection product category/View SQL.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace View_SQL_connection_product_category
+{
+    public partial class View_SQL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            SqlConnection SQLConn = new SqlConnection("Data Source=DESKTOP-3LUSQFK\\SQLEXPRESS01;Initial Catalog=NorthWind;Integrated Security=True");
+            SQLConn.Open();
+            SqlCommand cmd=new SqlCommand("Select * from Viewbolonizm where CategoryName='"+txbox1.Text+"'",SQLConn);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter adapter= new SqlDataAdapter(cmd);
+            DataTable tb=new DataTable();
+            adapter.Fill(tb);
+
+            if (tb.Rows.Count > 0)
+            {
+
+                GridView1.DataSource = tb;
+                GridView1.DataBind();
+                lbl2.Text = "";
+            }
+            else
+            {
+                lbl2.Text = "Not Found";
+                GridView1.DataSource = null;
+                GridView1.DataBind();
+            }
+        }
+        
+
+        protected void txbox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
